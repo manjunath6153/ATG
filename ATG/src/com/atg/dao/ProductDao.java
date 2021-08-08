265ee7e85307d4f856382482dfb169db52e7dd42
manjunath6153 <40025600+manjunath6153@users.noreply.github.com>
2021-08-08 15:30:51 +0530

    Delete ATG directory

--- ATG/src/com/atg/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.atg.dao;
-
-import com.atg.beans.Product;
-
-public interface ProductDao {
-	int addProduct(Product product);
-	// int updateProduct(Product product);
-	int deleteProduct(Product product);
-}
